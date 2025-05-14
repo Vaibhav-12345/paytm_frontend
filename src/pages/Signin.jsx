9c4554747b2e52aa732cc3cc7853d7c01ef5a9c0
Vaibhav-12345 <vaibhavmgs99@gmail.com>
2025-05-14 18:00:51 +0530

    import path wrong given

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -3,7 +3,7 @@ import Header from '../components/Header'
 import Subheading from '../components/Subheading'
 import Input from '../components/Input'
 import Button from '../components/Button'
-import Footersubheading from '../components/footersubheading'
+import Footersubheading from '../components/Footersubheading'
 import { useNavigate } from 'react-router-dom'
 
 import axios from 'axios'
